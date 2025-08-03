c03fb885929046e0634e56c3e980a76cd4d9a577
Rıdvan Güneş <gunes0070@hotmail.com>
2025-08-03 13:51:55 +0300

    added project framer motion animation package

--- app/(root)/courses/page.tsx
+++ app/(root)/courses/page.tsx
@@ -1,5 +1,17 @@
+"use client";
+
+import { motion } from "motion/react";
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+    >
+      <div>page</div>
+    </motion.div>
+  );
 };
 
 export default page;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,6 +10,7 @@ import Hero from "@/components/landing/Hero";
 import Stats from "@/components/landing/Stats";
 import SuccessStories from "@/components/landing/SuccessStories";
 import Testimnonials from "@/components/landing/Testimnonials";
+import { motion } from "motion/react";
 
 export default function Home() {
   return (
@@ -18,28 +19,78 @@ export default function Home() {
       <Header />
 
       {/* HERO SECTION */}
+
       <Hero />
 
       {/* FEATURED COURSES */}
-      <FeaturedCourses />
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: false, amount: 0.6 }}
+        transition={{ duration: 0.6 }}
+      >
+        <FeaturedCourses />
+      </motion.div>
 
       {/* WHY CHOOSE US */}
-      <About />
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: false, amount: 0.6 }}
+        transition={{ duration: 0.6 }}
+      >
+        <About />
+      </motion.div>
 
       {/* STATS SECTION */}
-      <Stats />
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: false, amount: 0.6 }}
+        transition={{ duration: 0.6 }}
+      >
+        <Stats />
+      </motion.div>
 
       {/* TESTIMONIALS SECTION */}
-      <Testimnonials />
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: false, amount: 0.6 }}
+        transition={{ duration: 0.6 }}
+      >
+        <Testimnonials />
+      </motion.div>
 
       {/* SUCCESS STORIES SECTION */}
-      <SuccessStories />
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: false, amount: 0.6 }}
+        transition={{ duration: 0.6 }}
+      >
+        <SuccessStories />
+      </motion.div>
 
       {/* COMBINED INSTRUCTOR & CTA SECTION */}
-      <CtaAndInstructorSection />
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: false, amount: 0.6 }}
+        transition={{ duration: 0.6 }}
+      >
+        <CtaAndInstructorSection />
+      </motion.div>
 
       {/* FAQ SECTION */}
-      <FAQ />
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: false, amount: 0.6 }}
+        transition={{ duration: 0.6 }}
+      >
+        <FAQ />
+      </motion.div>
 
       {/* FOOTER */}
       <Footer />
